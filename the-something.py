85528b302a95c03ded3205ec5c0724a572b14471
mrd104 <56087582+mrd104@users.noreply.github.com>
2019-10-02 13:17:46 -0500

    Update the-something.py
    added function to print name backwards

--- the-something.py
+++ the-something.py
@@ -1,6 +1,12 @@
+def printBackwards(name, i):
+	if i == -1:
+		return
+	print(name[i], end = '')
+	printBackwards(name, i - 1)
+
 name=input("Enter your name here")
 back=input("Do you want your name printed backwards? y/n")
-if back.equals("y"):
-	printBackwards(name)	
+if back == "y":
+	printBackwards(name, len(name) - 1)	
 else:
-	print("Thats ok ",name)	
+	print("Thats ok", name)	
